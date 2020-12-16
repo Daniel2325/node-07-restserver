3512a290456a161bb921193661385d53b03ad395
daniel_232526 <dan.iban232526@gmail.com>
2020-12-16 12:00:28 -0500

    Primero commit

--- server.js
+++ server.js
@@ -1,45 +1,49 @@
 require('./server/config')
 
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-Parser');
+
 const app = express();
-const bodyParser = require('body-parser');
 
-//parse application 
-app.use(bodyParser.urlencoded({extended:false}))
-
-//pars aplication/json
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function(req,res) { // GET ME SIRVE PARA CONSULTAR
-    res.json("get Usuario")
+app.get('/usuario', function (req,res){
+    //res.send("Hola mundo!");
+    res.json("get usuario");
 });
 
-app.post('/usuario', function(req,res) { //*POST PARA CREAR
-    let body =req.body;
-    if (body.nombre === undefined){
+app.post('/usuario', function (req,res){
+    let body = req.body;
+
+    if(body.nombre === undefined){
         res.status(400).json({
             ok: false,
-            mensaje: "eL NOMBRE DE USUAARIO ES REQUERIDO "
+            mensaje: "El nombre del usuario es requerido"
         });
-        
-    }else {
+    }else{
         res.json({
             body
-        }); 
+        });
     }
-    
 });
-app.put('/usuario/:id', function(req,res) { //MODIFICAR
-    let id = req.params.id;
+
+app.put('/usuario/:id', function (req,res){
+    let id=req.params.id;
+
     res.json({
         id
     });
 });
-app.delete('/usuario', function(req,res) { //ELIMINAR
-    res.json("delete Usuario")
-});
-app.listen(process.env.PORT,() =>{
-    console.log("Escuchando en el puerto:",PORT);
+
+app.delete('/usuario', function (req,res){
+    //res.send("Hola mundo!");
+    res.json("delete usuario");
 });
 
-
+app.listen(process.env.PORT, () => {
+    console.log("Escuchando en el puerto: ", process.env.PORT);
+});
